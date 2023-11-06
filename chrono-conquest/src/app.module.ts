4248f224f7d3093ae915af7d1d1740ee4e51ba21
augustocf94puc <47642058+augustocfreitas@users.noreply.github.com>
2023-11-06 19:16:50 -0300

    Set up linter and Database

--- chrono-conquest/src/app.module.ts
+++ chrono-conquest/src/app.module.ts
@@ -1,9 +1,20 @@
-import { AccountModule } from './modules/account/account.module';
-import { AccountController } from './controllers/account/account.controller';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+import { AccountController } from './controllers/account/account.controller';
+import { DatabaseModule } from './database/database.module';
+import { AccountModule } from './modules/account/account.module';
+
+dotenv.config();
 
 @Module({
-  imports: [AccountModule],
+  imports: [
+    AccountModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AccountController],
   providers: [],
 })
